252f4eeb5f675e121b26c16fb1bd418cf8e3d80c
BT_1N3_26 <f>
2019-01-07 10:50:05 -0500

    d

--- src/Activity1/Card.java
+++ src/Activity1/Card.java
@@ -35,6 +35,7 @@ public class Card {
      */
     public Card(String cardRank, String cardSuit, int cardPointValue) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+        suit = cardSuit; rank = cardRank; pointValue = cardPointValue;
     }
 
 
@@ -44,7 +45,7 @@ public class Card {
      */
     public String suit() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+        return suit;
     }
 
     /**
@@ -53,7 +54,7 @@ public class Card {
      */
     public String rank() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+        return rank;
     }
 
     /**
@@ -62,7 +63,7 @@ public class Card {
      */
     public int pointValue() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+        return pointValue;
     }
 
     /** Compare this card with the argument.
@@ -73,7 +74,10 @@ public class Card {
      */
     public boolean matches(Card otherCard) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+        if (suit.equals(otherCard.suit()) && rank.equals(otherCard.rank()) && pointValue == otherCard.pointValue()) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -89,7 +93,7 @@ public class Card {
     @Override
     public String toString() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-
+        return rank + " of " + suit + " ( point value = " + pointValue + ")";
     }
 }
 
--- src/Activity1/CardTester.java
+++ src/Activity1/CardTester.java
@@ -4,4 +4,7 @@ package Activity1;
  * Created by Teacher on 1/7/2019.
  */
 public class CardTester {
+    public static void main(String[] args) {
+        Card aceOfHearts = new Card("ace", "hearts", 500);
+    }
 }
